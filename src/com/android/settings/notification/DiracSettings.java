ddafe2e572e0e305d9934e1c522217b667b13f93
sayan7848 <sayan7848@gmail.com>
2019-03-02 17:38:58 +0000

    Revert "Dirac : Show a toast message for selected choice"
    This reverts commit cb747286b4c8fbc6b827c9f7b198fa3d247c04ff.
    
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- src/com/android/settings/notification/DiracSettings.java
+++ src/com/android/settings/notification/DiracSettings.java
@@ -54,7 +54,6 @@ public class DiracSettings extends SettingsPreferenceFragment {
 
         mHeadsetType.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
           public boolean onPreferenceChange(Preference preference, Object newValue) {
-            Toast.makeText(mContext,"Applied HeadSetType : " + String.valueOf(newValue),Toast.LENGTH_SHORT).show();
             int val = Integer.parseInt(newValue.toString());
             AudioEnhancerService.du.setHeadsetType(mContext, val);
             return true;
@@ -63,7 +62,6 @@ public class DiracSettings extends SettingsPreferenceFragment {
 
         mMusicMode.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
           public boolean onPreferenceChange(Preference preference, Object newValue) {
-            Toast.makeText(mContext,"Applied Music Mode : " + String.valueOf(newValue),Toast.LENGTH_SHORT).show();
             int val = Integer.parseInt(newValue.toString());
             AudioEnhancerService.du.setMode(mContext, val);
             return true;
@@ -72,7 +70,7 @@ public class DiracSettings extends SettingsPreferenceFragment {
 
         mDiracPreset.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
           public boolean onPreferenceChange(Preference preference, Object newValue) {
-            Toast.makeText(mContext,"Applied Level : " + String.valueOf(newValue),Toast.LENGTH_SHORT).show();
+            Toast.makeText(mContext,String.valueOf(newValue),Toast.LENGTH_SHORT).show();
             AudioEnhancerService.du.setLevel(mContext, String.valueOf(newValue));
             return true;
           }
